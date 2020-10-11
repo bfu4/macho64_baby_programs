7ea7e6e8c08afc91ee0780c60381acd72b45d344
bfu4 <46942366+bfu4@users.noreply.github.com>
2020-10-11 00:50:49 -0400

    better commenting

--- hello/hello.asm
+++ hello/hello.asm
@@ -1,15 +1,21 @@
-
-SECTION .data
-	uwu:	db "uwu ^w^ <3", 0	; C strings need 0 byte at end
-
-SECTION .text
-	global		_main
-	extern		_puts
-	
-
-	_main:				; make label available to linker
-		push	rbx		; call stack must b aligned
-		lea	rdi, [rel uwu]	; first arg is address of message
-		call	_puts		; puts(hi)
-		pop	rbx		; fix up stack before returning
-		ret
+;----------------------------------------------;;--------------------------------------------;;
+;		  hello world		       ;;		 the comment		     ;;
+;		   nasm (x64)		       ;;		   section		     ;;
+;----------------------------------------------;;--------------------------------------------;;
+					       ;;					     ;;
+SECTION .data				       ;;				   	     ;;
+	uwu:	db "uwu ^w^ <3", 0	       ;;       + C strings need 0 byte at end	     ;;
+					       ;;--------------------------------------------;;
+SECTION .text				       ;;					     ;;
+	global		_main		       ;;		  written in		     ;;
+	extern		_puts		       ;;		    nano		     ;;
+					       ;;					     ;;
+					       ;;--------------------------------------------;;
+	_main:				       ;;	+ make label available to linker     ;;
+		push	rbx		       ;; 	  + call stack must b aligned        ;;
+		lea	rdi, [rel uwu]	       ;;      + first arg is address of message     ;;
+		call	_puts		       ;; 		+ puts(hi)		     ;;
+		pop	rbx		       ;; 	+ fix up stack before returning      ;;
+		ret			       ;;--------------------------------------------;;
+					       ;;				  	     ;;
+;--------------- end of code ------------------;;------------- end of comments --------------;;
